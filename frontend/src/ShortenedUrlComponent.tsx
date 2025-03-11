527c2f29673111f00c635ecbe4c0fd353b8b9626
Guilherme Pereira <guilhermepereira2917@gmail.com>
2025-03-11 08:30:16 -0300

    improve shortened url css

--- frontend/src/ShortenedUrlComponent.tsx
+++ frontend/src/ShortenedUrlComponent.tsx
@@ -6,7 +6,7 @@ interface ShortenedUrlProps {
 
 export default function ShortenedUrlComponent({ shortenedUrl }: ShortenedUrlProps) {
   const url = shortenedUrl.url;
-  const formattedExpiresAt = new Date(shortenedUrl.expiresAt).toLocaleString();
+  const formattedExpiresAt = new Date(shortenedUrl.expiresAt).toLocaleString(window.navigator.language);
 
   const handleCopyClick = () => {
     navigator.permissions.query({ name: "clipboard-write" as PermissionName }).then((result) => {
@@ -21,14 +21,14 @@ export default function ShortenedUrlComponent({ shortenedUrl }: ShortenedUrlProp
   }
 
   return (
-    <div className="text-center">
+    <div className="flex flex-col items-center justify-center max-w-[700px] w-lvw">
       <h2 className="text-green-500">URL Shortened Successfully!</h2>
-      <div className="flex gap-2">
-        <p>Your shortened URL: <a href={url} target="_blank" className="text-blue-500 underline">{url}</a></p>      
-        <button onClick={handleCopyClick} className="bg-[url(/assets/copy-icon.svg)] bg-contain bg-no-repeat bg-center p-3 outline rounded cursor-pointer active:bg-gray-200"/>
+      <div className="flex justify-center items-center gap-2">
+        <p className="block text-wrap whitespace-pre-wrap text-center">Your shortened URL: <a href={url} target="_blank" className="break-all text-wrap text-blue-500 underline">{url}</a></p>      
+        <button onClick={handleCopyClick} className="bg-[url(/assets/copy-icon.svg)] w-4 h-4 bg-contain bg-no-repeat bg-center p-3 outline rounded cursor-pointer active:bg-gray-200"/>
       </div>
       <p className="text-gray-500 text-sm">Expires on: {formattedExpiresAt}</p>
-      <button onClick={handleBackClick} className="bg-black text-white font-bold p-2 w-full cursor-pointer rounded">Shorten another link</button>
+      <button onClick={handleBackClick} className="bg-black text-white font-bold p-2 w-full cursor-pointer rounded">Shorten another URL</button>
     </div>
   );
 }
